de37d7f0b39956ed9604356e15cbfba475fce0c0
SolveForX24 <86010571+SolveForX24@users.noreply.github.com>
2021-10-21 10:42:41 -0400

    Update Worksheets.cpp
    Finished 1 and 2, started 3

--- Lab 3.0 Conditionals Coding/Lab 3.0 Conditionals Coding/Worksheets.cpp
+++ Lab 3.0 Conditionals Coding/Lab 3.0 Conditionals Coding/Worksheets.cpp
@@ -2,14 +2,24 @@
 #include <iostream>
 #include <string>
 
+/*
+	Name: Xander Russell
+	Date: 10/21/20
+	Assignment: Lab 2.0 Conditionals
+	Extra: Detailed in each section
+*/
 using namespace std;
 
+string dummy = "a";
 
 void worksheet1(string wsName) {
     cout << endl << "worksheet: " << wsName << endl << endl;
     
 	// Logical Operators worksheet
 
+
+	// Part 1: Dogs and Cats
+	// EXTRA: Has a check for if you have 0 dogs or cats, and if you have a medley of dogs and cats, also prints out for that.
 	int dogs;
 	int cats;
 
@@ -19,22 +29,113 @@ void worksheet1(string wsName) {
 	cout << "How many cats do you have? ";
 	cin >> cats;
 
-	if (dogs == cats) {
-		cout << "Do your cats and dogs get along?";
+	if (cats == 0 && dogs == 0) {
+		cout << "No pets? D:\n\n";
 	}
-	else if (dogs > cats) {
+	else if (dogs == cats) {
+		cout << "Do your cats and dogs get along?\n\n";
+	}
+	else if (dogs > 0 && cats == 0) {
+		cout << "Guess you're a dog person!\n\n";
+	}
+	else if (cats > 0 && dogs == 0) {
+		cout << "Guess you're a cat person!\n\n";
+	}
+	else {
+		cout << "What a medley!\n\n";
+	}
 
+	// Part 2: Free money!
+
+	int moneyAmount = 0;
+
+	// Reading in input, running comparisons based on that input
+	cout << "Please enter an integer between 50 and 100: ";
+	cin >> moneyAmount;
+	if (moneyAmount >= 50 && moneyAmount <= 100) {
+		cout << "Congratulations! You earn $" << moneyAmount << " because you entered a number in the range!\n\n";
 	}
+	else {
+		cout << "You didn't enter a number in range D: no money for you ):\n\n";
+	}
+
+	getline(cin, dummy);
 }
 
 void worksheet2(string wsName) {
-    std::cout << endl << "worksheet: " << wsName << endl << endl;
-    //Put all code for worksheet 2 here
+    cout << endl << "worksheet: " << wsName << endl << endl;
+    
+	// Working with If Worksheet
+
+	// Part 1: Heinz Number
+	// EXTRA: Adds a check for the number 13
+	int foodNum = 0;
+
+	// Reading in input, running comparisons
+	cout << "Please enter an integer: ";
+	cin >> foodNum;
+	if (foodNum == 57) {
+		cout << "Hey that's the Heinz number!";
+	}
+	else if (foodNum == 13) {
+		cout << "Why did you choose the unlucky number D:";
+	}
+	else {
+		cout << "Just a plain old number |:";
+	}
+
+	// Part 2: Number in Range
+	int numInRangeCheck = 0;
+
+	// Reading in input, running comparisons
+	cout << "\nPlease enter an integer within the range of 5 and 10, inclusive: ";
+	cin >> numInRangeCheck;
+
+	if (numInRangeCheck > 10) {
+		cout << "That number is too big!\n\n";
+	}
+	else if (numInRangeCheck < 5) {
+		cout << "That number is too low!\n\n";
+	}
+	else if (numInRangeCheck == 7) {
+		cout << "Lucky number! :D\n\n";
+	}
+	else {
+		cout << "Great! A number in range.\n\n";
+	}
+	
+	getline(cin, dummy);
+
 }
 
 void worksheet3(string wsName) {
-    std::cout << endl << "worksheet: " << wsName << endl << endl;
-    //Put all code for worksheet 3 here
+    cout << endl << "worksheet: " << wsName << endl << endl;
+    
+	// If Else Worksheet
+
+	// Part 1: Can You Drive?
+	// EXTRA: If you are less then 4 years old, has funny prompt.
+	string name;
+	int age = 0;
+
+	// Reading in input 
+	cout << "What is your name? ";
+	getline(cin, name);
+
+	cout << "What is your age? ";
+	cin >> age;
+	
+	// Making comparisons
+	if (age >= 16) {
+		cout << "It's scary, " << name << ", you can drive now!\n\n";
+	}
+	else if (age < 4) {
+		cout << "How are you using this program?\n\n";
+	}
+	else {
+		cout << "You must wait " << 16 - age << " years until you can drive.\n\n";
+	}
+	getline(cin, dummy);
 }
 
 void worksheet4(string wsName) {
