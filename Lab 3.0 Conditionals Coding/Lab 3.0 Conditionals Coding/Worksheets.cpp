11db03cfbe4dc216f77bf07aa00aad1d7b8ffa6d
SolveForX24 <86010571+SolveForX24@users.noreply.github.com>
2021-10-27 10:41:42 -0400

    Update Worksheets.cpp
    worksheets 3 and 4 done

--- Lab 3.0 Conditionals Coding/Lab 3.0 Conditionals Coding/Worksheets.cpp
+++ Lab 3.0 Conditionals Coding/Lab 3.0 Conditionals Coding/Worksheets.cpp
@@ -1,6 +1,7 @@
 #include "Worksheets.h"
 #include <iostream>
 #include <string>
+#include <iomanip>;
 
 /*
 	Name: Xander Russell
@@ -145,16 +146,17 @@ void worksheet3(string wsName) {
 	cin >> integerToCheck;
 
 	// Making comparisons
-	if ((integerToCheck & 2) == 1) {
-		cout << "Your number is even!\n\n";
+	if ((integerToCheck % 2) == 1) {
+		cout << "Your number is odd!\n\n";
 	}
 	else {
-		cout << "Your number is odd!\n\n";
+		cout << "Your number is even!\n\n";
 	}
 
 	// Part 3: Telegrams
 
 	double wordsInTelegram;
+	cout.setf(ios::fixed);
 
 	// Reading in input
 	cout << "How many words are in your telegram? ";
@@ -166,16 +168,140 @@ void worksheet3(string wsName) {
 		cout << "Your telegram costs $8.50.\n\n";
 	}
 	else {
-		cout << "Your telegram costs $" << 8.5 * ((wordsInTelegram - 15) * .25) << ".\n\n";
+		cout << "Your telegram costs $" << setprecision(2) << 8.5 + ((wordsInTelegram - 15) * .25) << ".\n\n";
 	}
 
-	// Part 4: 
+	// Part 4: Song Qualifications 
+
+	int birthYear;
+	string fruit;
+
+	// Reading in input to determine qualification for the juke box rally and song to print.
+
+	cout << "What is your birth year? ";
+	cin >> birthYear;
+
+	if (birthYear < 1970) {
+		cout << "You qualify for the Juke Box Rally\n";
+	}
+
+	cout << "What is your favorite fruit in lowercase? ";
+	cin >> fruit;
+
+	if (fruit == "strawberry") {
+		cout << "Strawberry Fields Forever\n\n";
+	}
+	else {
+		cout << "Bye, bye Miss American Pie\n\n";
+	}
+
+	// Part 5: Order Of Ints
+
+	int intOne;
+	int intTwo;
+
+	// Getting input, running comparisons.
+	cout << "Please enter two integers: ";
+	cin >> intOne >> intTwo;
+
+	cout << "In ascending order: ";
+	if (intOne > intTwo) {
+		cout << intTwo << " " << intOne << "\n\n";
+	}
+	else if (intTwo > intOne) {
+		cout << intOne << " " << intTwo << "\n\n";
+	}
+	else {
+		cout << "The integers are equal.\n\n";
+	}
+
+	// Part 6: Art Fee
+
+	int ageArt;
+
+	// Getting input, running comparisons.
+	cout << "What is your age? ";
+	cin >> ageArt;
+
+	if (ageArt <= 5) {
+		cout << "Entry fee: $0.00. You're free because you are so young.\n\n";
+	}
+	else if (ageArt >= 65) {
+		cout << "Entry fee: $1.50.\n\n";
+	}
+	else {
+		cout << "Entry fee: $2.50.\n\n";
+	}
+
 	getline(cin, dummy);
 }
 
 void worksheet4(string wsName) {
-    std::cout << endl << "worksheet: " << wsName << endl << endl;
-    //Put all code for worksheet 4 here
+	std::cout << endl << "worksheet: " << wsName << endl << endl;
+
+	// More Working with If-Else
+
+	// Part 1: Password 
+
+	string password = "password";
+	string attempt;
+
+	// Input, comparisons
+	cout << "What is the password? ";
+	getline(cin, attempt);
+
+	if (attempt == password) {
+		cout << "Access granted.\n\n";
+	}
+	else {
+		cout << "WHO ARE YOU? I'M CALLING THE COPS HACKER\n\n";
+	}
+
+	// Part 2: Movie Length
+	// EXTRA: Shows the difference in character length for the movie names.
+
+	string movieOne, movieTwo;
+	int movieOneLength, movieTwoLength;
+
+	// We read in strings, then add it's length to integers.
+	cout << "What is your first movie? ";
+	getline(cin, movieOne);
+	movieOneLength = movieOne.length();
+
+	cout << "What is your second movie? ";
+	getline(cin, movieTwo);
+	movieTwoLength = movieTwo.length();
+
+	// Comparing those length integers
+	if (movieOneLength > movieTwoLength) {
+		cout << "Your first movie, " << movieOne << ", is longer then your second movie, " << movieTwo << ", by " << movieOneLength - movieTwoLength << " characters.\n\n";
+	}
+	else if (movieTwoLength > movieOneLength) {
+		cout << "Your second movie, " << movieTwo << ", is longer then your first movie, " << movieOne << ", by " << movieTwoLength - movieOneLength << " characters.\n\n";
+	}
+	else {
+		cout << "Your movies are equal in character length.\n\n";
+	}
+
+	// Part 3: Reformatting Hard Drives
+
+	string continueOn;
+
+	// Reading in input, comparing to strings.
+	cout << "This program may reformat your hard drive. Do you want to continue? (y/n) ";
+	getline(cin, continueOn);
+
+	if (continueOn == "y") {
+		cout << "So long, data! :)\n\n";
+	}
+	else if (continueOn == "n") {
+		cout << "Caught in the act! D: Your data is safe.\n\n";
+	}
+	else {
+		cout << "... I'll take that as a yes! :)\n\n";
+	}
+
+
 }
 
 void worksheet5(string wsName) {
