7e20b346614296e071b83753a1abadc0f62456ab
SolveForX_ <86010571+SolveForX24@users.noreply.github.com>
2021-11-24 09:24:41 -0500

    Update main.cpp

--- 4.0 Alternate Assignment Code/4.0 Alternate Assignment/main.cpp
+++ 4.0 Alternate Assignment Code/4.0 Alternate Assignment/main.cpp
@@ -1,12 +1,11 @@
-// You need to rename this project using the following Steps
-// 1. Rename this replit
-// 2. Select "Rename" and rename it appropriately. For example DiGiovanna_WS_Files
+
 
 // Add your header.
 
 #include <iostream>
 #include <string>
 // Add all other needed header files
+#include <iomanip>
 
 using namespace std;
 
@@ -26,7 +25,7 @@ int main()
 	string shutDownPassword;
 	string shouldAbandon;
 
-	int orderCount = 0;
+	int orderCount = 999;
 	
 	int hotdogCount = 0;
 	int candyCount = 0;
@@ -34,17 +33,20 @@ int main()
 	int popcornCount = 0;
 	int pizzaCount = 0;
 
+	float price = 0;
+
+	cout.setf(ios::fixed);
+	cout << setprecision(2);
 
 	while (running) {
 		
-		orderCount = 0;
-
 		hotdogCount = 0;
 	    candyCount = 0;
 	    drinkCount = 0;
 		popcornCount = 0;
 	    pizzaCount = 0;
 
+		price = 0;
 
 		running = true;
 		ordering = true;
@@ -52,70 +54,120 @@ int main()
 
 		cout << "What is your name? ";
 		getline(cin, name);
-		//getline(cin, dummy);
 		
 		while (ordering) {
-			cout << "Please make an order:\n\n(B)everage	$5.00\n(C)andy      	$2.25\n(H)ot Dog	$7.00\n(P)opcorn	$6.75\nP(i)zza	        $4.50\n\nPlease enter one of the letters in the parentheses for your order: ";
-			cin >> order;
+			cout << "\n\nPlease make an order:\n\n(B)everage	$5.00\n(C)andy      	$2.25\n(H)ot Dog	$7.00\n(P)opcorn	$6.75\nP(i)zza	        $4.50\n\nPlease enter one of the letters in the parentheses for your order: ";
+			getline(cin, order);
 
-			//order = lowerCase(order);
-
-			cout << order;
-
-			if (order == "b") {
+			if (order == "b" || order == "B") {
 				cout << "Your order for a Beverage has been processed!";
 				drinkCount++;
+				price += 5.00;
 			}
-			else if (order == "c") {
+			else if (order == "c" || order == "C") {
 				cout << "Your order for a Candy has been processed!";
 				candyCount++;
+				price += 2.25;
 			}
-			else if (order == "h") {
+			else if (order == "h" || order == "H") {
 				cout << "Your order for a Hot Dog has been processed!";
 				hotdogCount++;
+				price += 7.00;
 			}
-			else if (order == "p") {
+			else if (order == "p" || order == "P") {
 				cout << "Your order for a Popcorn has been processed!";
 				popcornCount++;
+				price += 6.75;
 			}
-			else if (order == "i") {
+			else if (order == "i" || order == "I") {
 				cout << "Your order for a Pizza has been processed!";
 				pizzaCount++;
+				price += 4.50;
 			}
-			else if (order == "q") {
+			else if (order == "q" || order == "Q") {
 				cout << "Please enter the shut-down password: ";
-				getline(cin, shutDownPassword);
-				getline(cin, dummy);
+				
+				getline(cin, shutDownPassword);	
 
 				if (shutDownPassword == "ShutMeDown!") {
 					shutDown = true;
 					break;
+				} else if (shutDownPassword != "ShutMeDown!") {
+					shutDownPassword == "a";
+					continue;
 				}
 			}
 			else {
-				cout << "That was not a proper menu. Would you like to try again, or would you like me to cancel your order? (Try Again = T, Leave = L) ";
-				cin >> shouldAbandon;
-				//shouldAbandon = lowerCase(shouldAbandon);
-
+				cout << "\nThat was not a proper menu order. Would you like to try again, or would you like me to cancel your order? (Try Again = T, Leave = L) ";
+				
+				getline(cin, shouldAbandon);
+				if (shouldAbandon == "L" || shouldAbandon == "l") {
+					abandoned = true;
+					cout << "\nAbandoning order\n\n";
+					break;
+				}
+				else if (shouldAbandon == "T" || shouldAbandon == "t") {
+					continue;
+				}
 			}
 
-			cout << "\nWould you like to order another item? (y/n) ";
-			getline(cin, dummy);
+			if (shutDown == true) {
+				break;
+			} 
+
+			cout << "\n\nWould you like to order another item? (y/n) ";
+			
 			getline(cin, continueOrder);
 
-			if (continueOrder == "n") {
-				cout << "Heading to ordar summary: ";
+			if (continueOrder == "n" || continueOrder == "N") {
+				cout << "\nHeading to ordar summary: ";
 				ordering = false;
 			}
-			else if (continueOrder == "y") {
+			else if (continueOrder == "y" || continueOrder == "Y") {
 			}
 			else {
-
+				cout << "\n\nOption not determined. Going to order menu.\n";
 			}
+			
 		}
 
 		if (shutDown) {
+			cout << "\n\nPassword accepted. Shutting down due to admin access.\n\n";
 			break;
+		}
+		else if (abandoned) {
+			cout << "Order abandoned\n\n";
+			continue;
+		}
+	
+		else {
+			cout << "\n\nPlease deposit $" << price << " while I process your order.\n";
+			if (drinkCount >= 1) {
+				cout << "\n" << drinkCount << " Beverage(s)";
+			}
+			
+			if (candyCount >= 1) {
+				cout << "\n" << candyCount << " Hot Dog(s)";
+			}
+
+			if (hotdogCount >= 1) {
+				cout << "\n" << hotdogCount << " Hot Dog(s)";
+			}
+
+			if (popcornCount >= 1) {
+				cout << "\n" << popcornCount << " Hot Dog(s)";
+			}
+
+			if (pizzaCount >= 1) {
+				cout << "\n" << pizzaCount << " Hot Dog(s)";
+			}
+
+			cout << "\n\nThank you for your purchase!\n\n\n\n";
+			orderCount++;
+			if (orderCount >= 1000) {
+				cout << "\n\nShutting down to replenish materials.\n\n";
+				break;
+			}
 		};
 
 	}
