8fb4ba9cc37f1bee18d7e35af0536ad9072675e1
SolveForX_ <86010571+SolveForX24@users.noreply.github.com>
2021-12-10 10:44:19 -0500

    Update PiggyBank.h

--- PiggyBankOOP/FirstOOP/PiggyBank.h
+++ PiggyBankOOP/FirstOOP/PiggyBank.h
@@ -7,34 +7,38 @@
 #pragma once
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 
 class PiggyBank
 {
 private:
-	std::string name = "a";
-	float pennies = 0;
-	float nickels = 0;
-	float dimes = 0;
-	float quarters = 0;
-	float timeSaved = 0;
+	std::string name = "EMPTY";
+	int pennies = 0;
+	int nickels = 0;
+	int dimes = 0;
+	int quarters = 0;
+	int timeSaved = 0;
 public:
 	
 	// Constructors
 	PiggyBank();
-	PiggyBank(std::string, float, float, float, float, float);
+	PiggyBank(std::string, int, int, int, int, int);
 	
 	// Getters
 	std::string getName();
-	float getPennies();
-	float getNickels();
-	float getDimes();
-	float getQuarters();
-	float getTimeSaved();
+	int getPennies();
+	int getNickels();
+	int getDimes();
+	int getQuarters();
+	int getTimeSaved();
 
 	// Useful functions
 	float calculateTotal();
+	float calculateSavingRate();
+	float calculateSavingsPerYear();
+	std::string printSavingsStatement();
 
 	//friend std::ostream& operator<<(std::ostream&, const PiggyBank&);
 };
