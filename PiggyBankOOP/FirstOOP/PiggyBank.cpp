018b0fa91859c7ddba6519b3784d3dabb5727cc2
SolveForX_ <86010571+SolveForX24@users.noreply.github.com>
2021-12-14 10:44:16 -0500

    Update PiggyBank.cpp

--- PiggyBankOOP/FirstOOP/PiggyBank.cpp
+++ PiggyBankOOP/FirstOOP/PiggyBank.cpp
@@ -46,19 +46,32 @@ int PiggyBank::getTimeSaved() {
 	return timeSaved;
 }
 
+// This code found here: https://www.geeksforgeeks.org/rounding-floating-point-number-two-decimal-places-c-c/
+float PiggyBank::round(float var) {
+	// 37.66666 * 100 =3766.66
+	// 3766.66 + .5 =3767.16    for rounding off value
+	// then type cast to int so value is 3767
+	// then divided by 100 so the value converted into 37.67
+	float value = (int)(var * 100 + .5);
+	return (float)value / 100;
+}
+
 float PiggyBank::calculateTotal() {
 	float toReturn = ((pennies)+(nickels * 5) + (dimes * 10) + (quarters * 25))/100.0;
+	round(toReturn);
 	return toReturn;
 }
 
 float PiggyBank::calculateSavingRate() {
 	float toReturn = (calculateTotal()) / timeSaved;
+	round(toReturn);
 	return toReturn;
 }
 
 float PiggyBank::calculateSavingsPerYear() {
 	float toReturn = (52 / timeSaved) * (calculateTotal());
-	return toReturn
+	round(toReturn);
+	return toReturn;
 }
 
 string PiggyBank::printSavingsStatement() {
