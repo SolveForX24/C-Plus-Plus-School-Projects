4267afbee499cea2a77f9b86e044288aadef1c56
SolveForX_ <86010571+SolveForX24@users.noreply.github.com>
2021-12-20 10:46:34 -0500

    Update main.cpp

--- PiggyBankOOP/FirstOOP/main.cpp
+++ PiggyBankOOP/FirstOOP/main.cpp
@@ -24,29 +24,29 @@ int main()
 	
 
 	cout << "Hi, " << name << "! How many pennies have you saved? ";
-	while (!getValidInt(pennies)) {
+	while (!getValidIntNoNeg(pennies)) {
 		cout << "Sorry, that's not a proper penny amount. Please try again: ";
 	}
 	cout << "Great! How many nickels have you saved? ";
-	while (!getValidInt(nickels)) {
+	while (!getValidIntNoNeg(nickels)) {
 		cout << "Sorry, that's not a proper nickel amount. Please try again: ";
 	}
 	cout << "Awesome. How many dimes have you saved, " << name << "? ";
-	while (!getValidInt(dimes)) {
+	while (!getValidIntNoNeg(dimes)) {
 		cout << "Sorry, that's not a proper dime amount. Please try again: ";
 	}
 	cout << "Super! How many quarters have you saved? ";
-	while (!getValidInt(quarters)) {
+	while (!getValidIntNoNeg(quarters)) {
 		cout << "Sorry, that's not a proper quarters amount. Please try again: ";
 	}
 	cout << "We're almost done! How many weeks have you been saving, " << name << "? ";
-	while (!getValidInt(timeSaved)) {
+	while (!getValidIntNoNegNoZero(timeSaved)) {
 		cout << "Sorry, that's not a proper week amount. Please try again: ";
 	}
 
-	cout << "All done! Let me do some math for you...\n\n";
+	cout << setprecision(2) << "All done! Let me do some math for you...\n\nHere are your totals per coin type, " << name << ":\nQuarters: $" << (quarters * 25) / 100.0 << ", from " << quarters << " quarters.\nDimes: $" << (dimes * 10) / 100.0 << ", from " << dimes << " dimes.\nNickels: $" << (nickels * 5) / 100.0 << ", from " << nickels << " nickels.\nPennies: $" << pennies / 100.0 << ", from " << pennies << " pennies.\n\n";
 
 	PiggyBank bank(name, pennies, nickels, dimes, quarters, timeSaved);
-	cout << setprecision(2) << bank.printSavingsStatement();
+	bank.printSavingsStatement();
 
 }
