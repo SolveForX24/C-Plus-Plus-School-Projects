7cebf51e61556faf5e9779d42500b8fb3c04abf7
SolveForX_ <86010571+SolveForX24@users.noreply.github.com>
2021-12-22 10:44:47 -0500

    Finished OOP Version

--- PiggyBankOOP/FirstOOP/main.cpp
+++ PiggyBankOOP/FirstOOP/main.cpp
@@ -1,5 +1,12 @@
+/*
+	Name: Xander Russell
+	Date: 12/22/21
+	Lab: Piggy Bank (OOP Version)
+	Extra: Special messages for when you've saved over a year or for exactly a year.
+*/
+
+// Including the class and the error trapping
 #include <iostream>
-#include "FirstClass.h"
 #include "PiggyBank.h"
 #include "GetData.h"
 #include <iomanip> 
@@ -8,6 +15,7 @@ using namespace std;
 int main()
 {
 
+	// Setting up print precision and variables.
 	cout.setf(ios::fixed);
 
 	string name = "EMPTY";
@@ -19,33 +27,46 @@ int main()
 	
 	string dummy = "EMPTY";
 
+	// Getting name
 	cout << "Hello, there! What's your name? ";
 	getline(cin, name);
 	
+	// Getting each coin amount, and weeks saved, with error trapping. 
 
-	cout << "Hi, " << name << "! How many pennies have you saved? ";
-	while (!getValidIntNoNeg(pennies)) {
-		cout << "Sorry, that's not a proper penny amount. Please try again: ";
-	}
-	cout << "Great! How many nickels have you saved? ";
-	while (!getValidIntNoNeg(nickels)) {
-		cout << "Sorry, that's not a proper nickel amount. Please try again: ";
-	}
-	cout << "Awesome. How many dimes have you saved, " << name << "? ";
-	while (!getValidIntNoNeg(dimes)) {
-		cout << "Sorry, that's not a proper dime amount. Please try again: ";
-	}
-	cout << "Super! How many quarters have you saved? ";
-	while (!getValidIntNoNeg(quarters)) {
-		cout << "Sorry, that's not a proper quarters amount. Please try again: ";
-	}
-	cout << "We're almost done! How many weeks have you been saving, " << name << "? ";
-	while (!getValidIntNoNegNoZero(timeSaved)) {
-		cout << "Sorry, that's not a proper week amount. Please try again: ";
-	}
+	// Pennies
+	cout << "Hi, " << name << "! ";
+	do {
+		cout << "How many pennies have you saved? ";
+	} while (!getValidInt(pennies) || (pennies < 0));
 
+	// Nickels
+	cout << "Great! ";
+	do {
+		cout << "How many nickels have you saved? ";
+	} while (!getValidInt(nickels) || (nickels < 0));
+
+	// Dimes
+	cout << "Awesome. ";
+	do {
+		cout << "How many dimes have you saved, " << name << "? ";
+	} while (!getValidInt(dimes) || (dimes < 0));
+
+	// Quarters
+	cout << "Super! ";
+	do {
+		cout << "How many quarters have you saved? ";
+	} while (!getValidInt(quarters) || (quarters < 0));
+
+	// Time Saved
+	cout << "We're almost done! ";
+	do {
+		cout << "How many weeks have you been saving, " << name << "? ";
+	} while (!getValidInt(timeSaved) || (timeSaved < 0));
+
+	// Printing out the total monetary value of individual coin types.
 	cout << setprecision(2) << "All done! Let me do some math for you...\n\nHere are your totals per coin type, " << name << ":\nQuarters: $" << (quarters * 25) / 100.0 << ", from " << quarters << " quarters.\nDimes: $" << (dimes * 10) / 100.0 << ", from " << dimes << " dimes.\nNickels: $" << (nickels * 5) / 100.0 << ", from " << nickels << " nickels.\nPennies: $" << pennies / 100.0 << ", from " << pennies << " pennies.\n\n";
 
+	// Creating piggy bank object with input, then using it's print function.
 	PiggyBank bank(name, pennies, nickels, dimes, quarters, timeSaved);
 	bank.printSavingsStatement();
 
